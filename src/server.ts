f4416284de809d357b9c792cf7e8f3c37bb4db97
Richard Shephard <richards@sahomeloans.com>
2020-08-19 13:49:53 +0200

    Update for body-parser and cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import os from 'os';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import config from './configuration/config';
 import { apiRoutes } from './routes';
 
@@ -9,6 +11,9 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
 
 apiRoutes.forEach(route => {
   app.use(route.path, route.routerCb);
