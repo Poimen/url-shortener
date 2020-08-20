27b7a96bab249d4a88da4c7f026db972faaa466c
Richard Shephard <richards@sahomeloans.com>
2020-08-20 15:24:19 +0200

    Minor cleanup

--- src/controllers/admin/controller.ts
+++ src/controllers/admin/controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { MongoDatabaseConnector } from '../../infrastructure/mongodb';
 import { UrlService } from '@/domain/urlService';
 import { CreateShortUrlDto } from './models/createShortUrlDto';
-import { validationResult } from 'express-validator';
 
 const databaseConnector = new MongoDatabaseConnector();
 const urlService = new UrlService(databaseConnector.makeUrlShortener('url-shortener'));
@@ -13,19 +12,6 @@ export class Controller {
   }
 
   public async createShortUrl(req: Request, res: Response): Promise<void> {
-  //   CreateShortUrlDtoValidator.validate();
-  //   const errors = validationResult(req);
-  //   console.dir(errors);
-  //   if (!errors.isEmpty()) {
-  //     res
-  // //      .status(HttpStatusCodes.BAD_REQUEST)
-  //       .status(400)
-  //       .json({ errors: errors.array() });
-  //     return;
-  //   }
-
-
-    // TODO - add try/catch for common error handling
     const userUrl = new CreateShortUrlDto(req.body.url, req.body.validUntil);
     const url = await urlService.recordShortUrlVersion(userUrl);
 
